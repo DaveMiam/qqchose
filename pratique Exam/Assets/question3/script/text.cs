30a2b2dbf074211867218f7321f92688f13ccf70
B533P-21\533 <daloignon90@gmail.com>
2018-10-02 18:03:44 -0400

    as

---
+++ pratique Exam/Assets/question3/script/text.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class text : MonoBehaviour
+{
+    public float time = 2;
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(gameObject, time);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
